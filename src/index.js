6f48acb2bb4bb42592251cba2b7982cd06b16331
Katei <kateievans28@gmail.com>
2025-03-24 08:58:08 +0300

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 // Your code here
+document.addEventListener("DOMContentLoaded", () =>{
+    const baseURL ="http://localhost:3000/characters";
+    const characterBar = document.getElementById("character-bar");
+    const detailedInfo = document.getElementById("detailed-info");
+    const voteForm = document.getElementById("votes-form")
+    const resetbtn = document.getElementById("reset.btn")
+    
+    fetch(baseURL)
+    .then(response => response.json())
+    .then(characters => {characters.forEach(character =>)})
+})
