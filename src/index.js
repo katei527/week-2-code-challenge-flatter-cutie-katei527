b92d9a60a0562e9a1286e040569d37b38c7e8178
Katei <kateievans28@gmail.com>
2025-03-24 11:07:03 +0300

    added result

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 // Your code here
-
-let baseURL = "http://localhost:3000/characters"
+let baseURL = "https://flatter-katei-ch3v.vercel.app/characters"
 fetch(baseURL)
 .then (response => response.json())
 .then(data => displayName(data))
@@ -19,4 +18,9 @@ function displayImage (){
     let img = document.querySelector("img")
     img.scr = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHRsb215MTZlNjhkaG1odXh4cDJjaWx2dW5peG1lZzg4eTc4a2ttciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o8doVAxrMjXbIHaU0/giphy.gif'
 }
+displayImage()
 
+function Results  (){
+    const num = document.getElementById(vote-count)
+    num.innerHTML = `<h4>Total Votes: <span id="vote-count">Character's Votes</span></h4>`
+}
