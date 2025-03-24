402601b84b35c545feaa63f75a7f3ec2b9d1fce5
Katei <kateievans28@gmail.com>
2025-03-24 10:08:16 +0300

    updated link images

--- src/index.js
+++ src/index.js
@@ -33,3 +33,4 @@ function displayImage (){
     let img = document.querySelector("img")
     img.scr = 'https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExeHRsb215MTZlNjhkaG1odXh4cDJjaWx2dW5peG1lZzg4eTc4a2ttciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/3o8doVAxrMjXbIHaU0/giphy.gif'
 }
+
